908e23697df321d8e2501e6f81b876d3cd0fe7e7
Xiangzhi Sheng <xzsheng@msn.com>
2016-10-21 17:38:21 +0800

    Remove duplicated code

--- IoTHubConsole/Actions/QueryDevicesAction.cs
+++ IoTHubConsole/Actions/QueryDevicesAction.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using IoTHubConsole.Properties;
 using Microsoft.Azure.Devices;
 
@@ -13,36 +12,11 @@ namespace IoTHubConsole.Actions
 
             if (args.DeviceIds != null)
             {
-                await QueryDevicesByIds(client, args.DeviceIds);
+                await IoTHubHelper.QueryDevicesByIds(client, args.DeviceIds);
             }
             else
             {
-                await QueryDevicesByCondition(client, args.QueryCondition ?? "select * from devices");
-            }
-        }
-
-        static async Task QueryDevicesByCondition(RegistryManager client, string condition)
-        {
-            var result = client.CreateQuery(condition);
-            while (result.HasMoreResults)
-            {
-                var twins = await result.GetNextAsTwinAsync();
-
-                foreach (var twin in twins)
-                {
-                    var device = await client.GetDeviceAsync(twin.DeviceId);
-                    IoTHubHelper.OutputDevice(twin, device);
-                }
-            }
-        }
-
-        static async Task QueryDevicesByIds(RegistryManager client, IEnumerable<string> deviceIDs)
-        {
-            foreach (var deviceId in deviceIDs)
-            {
-                var twin = await client.GetTwinAsync(deviceId);
-                var device = await client.GetDeviceAsync(deviceId);
-                IoTHubHelper.OutputDevice(twin, device);
+                await IoTHubHelper.QueryDevicesByCondition(client, args.QueryCondition ?? "select * from devices");
             }
         }
     }
